454f83a0c2cf5bfef0ae5daa40b4e56caa9b2c61
rdsports42-hub <rdsports42@gmail.com>
2026-06-01 17:28:59 +0530

    Create main App.js entry point

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { AuthProvider } from './src/contexts/AuthContext';
+import RootNavigator from './src/navigation/RootNavigator';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <RootNavigator />
+        </AuthProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
